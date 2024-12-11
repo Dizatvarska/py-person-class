97c8559593c8331199d7ea639938d8eed1b85524
Diana Zatvarska <dizatvarska@gmail.com>
2024-12-11 13:13:37 -0800

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.husband = None
+        self.wife = None
+        Person.people[name] = self
+
+    def set_relationships(self, people: dict) -> dict:
+        if "wife" in people and people["wife"]:
+            self.wife = Person.people[people["wife"]]
+        elif "wife" in people:
+            if hasattr(self, "wife"):
+                del self.wife
+
+        if "husband" in people and people["husband"]:
+            self.husband = Person.people[people["husband"]]
+        elif "husband" in people:
+            if hasattr(self, "husband"):
+                del self.husband
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_objects = []
+    for person_data in people:
+        person = Person(person_data["name"], person_data["age"])
+        person_objects.append(person)
+
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        person.set_relationships(person_data)
+
+    return person_objects
